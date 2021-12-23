88e169e2020cf4c8f35a8da16dba3225e97e887f
KhadidjaArezki <khadidjaarezki999@gmail.com>
2021-12-23 20:41:06 +0100

    Add static middleware to index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 // Create a web server and register a callback to execute on each new request
 let notes = [
